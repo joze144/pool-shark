fa51b92a3a739b4600e3d568b5c42b4e74d87d04
jozhe <joze.kosmerl@gmail.com>
2018-04-15 18:00:33 +0800

    test fix

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Shark pool contracts
-Create a pool with deadline in which everyone can add Ethereum. 
-With adding Ethereum user gets points, which can be transferred 
+# Shark of The Pool contracts
+Create a pool with deadline in which everyone can add Ether. 
+With adding Ether user gets points, which can be transferred 
 to another user or kept by yourself. After deadline tokens 
-aren’t transferable anymore, and user with most points wins 
-and can withdraw Ethereum funds in the pool. 
+are not transferable anymore, and user with most points wins 
+and can withdraw Ethe funds in the pool. 
 
 
 ### FishToken contract
@@ -11,18 +11,18 @@ Represents points awarded for ETH deposit to the pool.
 Tokens can be created and transferred until deadline.
 
 Properties:
-- Transferable to whitelisted members (until deadline expires)
+- Transferable (until deadline expires)
 - Issued on deposit (until deadline expires)
 - Transfer event, issue event
 - Get current shark
 
 
 ### Pool contract
-Is a pool where ETH is being collected. 
+Is a pool where Ether is being collected. 
 It has it's own FishToken contract to keep track of contributions.
-Users can contribute ETH until deadline expires.
-After deadline expires, shark (winner) can withdraw all ETH funds collected.
+Users can contribute Ether until deadline expires.
+After deadline expires, shark (winner) can withdraw all Ether collected.
 
 Properties:
-- Deposit (add to whitelist, create tokens) (until deadline expires)
+- Deposit (create tokens) (until deadline expires)
 - Withdraw, only for shark (winner) (after deadline expires)
--- test/fishToken.js
+++ test/fishToken.js
@@ -44,24 +44,40 @@ contract('FishToken', async (accounts) => {
     await instance.transfer(user1, transfer1, {from: owner})
     assert.equal(await instance.currentShark(), user1)
     const t1balanceOwner = await instance.balanceOf(owner, {from: owner})
+    const t1balanceOwnerShouldBe = toIssue - transfer1
     const t1balanceUser1 = await instance.balanceOf(user1, {from: owner})
-    assert.equal(t1balanceOwner, (toIssue - transfer1).toString())
-    assert.equal(t1balanceUser1, transfer1.toString())
+    const t1balanceUser1ShouldBe = transfer1
+    assert.equal(t1balanceOwner, t1balanceOwnerShouldBe.toString())
+    assert.equal(t1balanceUser1, t1balanceUser1ShouldBe.toString())
 
     const transfer2 = 200
     await instance.transfer(owner, transfer2, {from: user1})
     assert.equal(await instance.currentShark(), owner)
     const t2balanceOwner = await instance.balanceOf(owner, {from: owner})
+    const t2balanceOwnerShouldBe = t1balanceOwnerShouldBe + transfer2
     const t2balanceUser1 = await instance.balanceOf(user1, {from: owner})
-    assert.equal(t2balanceOwner, (t1balanceOwner + transfer2).toString())
-    assert.equal(t2balanceUser1, (t1balanceUser1 - transfer2).toString())
+    const t2balanceUser1ShouldBe = t1balanceUser1ShouldBe - transfer2
+    assert.equal(t2balanceOwner, t2balanceOwnerShouldBe.toString())
+    assert.equal(t2balanceUser1, t2balanceUser1ShouldBe.toString())
 
     const transfer3 = 50
     await instance.transfer(user1, transfer3, {from: owner})
     assert.equal(await instance.currentShark(), owner)
     const t3balanceOwner = await instance.balanceOf(owner, {from: owner})
+    const t3balanceOwnerShouldBe = t2balanceOwnerShouldBe - transfer3
     const t3balanceUser1 = await instance.balanceOf(user1, {from: owner})
-    assert.equal(t3balanceOwner, (t2balanceOwner - transfer3).toString())
-    assert.equal(t3balanceUser1, (t2balanceUser1 + transfer3).toString())
+    const t3balanceUser1ShouldBe = t2balanceUser1ShouldBe + transfer3
+    assert.equal(t3balanceOwner, t3balanceOwnerShouldBe.toString())
+    assert.equal(t3balanceUser1, t3balanceUser1ShouldBe.toString())
+
+    const transfer4 = 500
+    await instance.transfer(user2, transfer4, {from: owner})
+    assert.equal(await instance.currentShark(), user2)
+    const t4balanceOwner = await instance.balanceOf(owner, {from: owner})
+    const t4balanceOwnerShouldBe = t3balanceOwnerShouldBe - transfer4
+    const t4balanceUser2 = await instance.balanceOf(user2, {from: owner})
+    const t4balanceUser2ShouldBe = transfer4
+    assert.equal(t4balanceOwner, t4balanceOwnerShouldBe.toString())
+    assert.equal(t4balanceUser2, t4balanceUser2ShouldBe.toString())
   })
 })
