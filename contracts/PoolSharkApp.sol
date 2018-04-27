45baca3b63f1ec86e00416e82f0c7045074d5c05
jozhe <joze.kosmerl@gmail.com>
2018-04-27 21:16:14 +0800

    removed unnecessary stuff in main contract

--- contracts/PoolSharkApp.sol
+++ contracts/PoolSharkApp.sol
@@ -2,27 +2,17 @@ pragma solidity ^0.4.21;
 
 import "./Pool.sol";
 import "./Ownable.sol";
+import "./iPoolSharkApp.sol";
 
-contract PoolSharkApp is Ownable {
-
-    address public owner;
-    address[] public pools;
+contract PoolSharkApp is iPoolSharkApp {
 
     function PoolSharkApp() public {
-        owner = msg.sender;
     }
-
-    event LogPoolCreated(address indexed _pool, string _name, uint256 _rate, uint256 _deadline);
 
     function createPool(string _name, uint256 _rate, uint256 _deadline) external returns (bool success) {
         address newPoolAddress = new Pool(_name, _rate, _deadline);
-        pools.push(newPoolAddress);
 
         emit LogPoolCreated(newPoolAddress, _name, _rate, _deadline);
         return true;
-    }
-
-    function getPools() external view returns (address[] _pools) {
-        return pools;
     }
 }
---
+++ contracts/iPoolSharkApp.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.21;
+
+contract iPoolSharkApp {
+    function createPool(string _name, uint256 _rate, uint256 _deadline) external returns (bool success);
+
+    event LogPoolCreated(address indexed _pool, string _name, uint256 _rate, uint256 _deadline);
+}
